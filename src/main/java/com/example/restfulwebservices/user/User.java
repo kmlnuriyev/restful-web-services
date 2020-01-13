8feb8cbf348ee96c58633a7d013fc31347fc1dd6
kmlnuriyev <homemobile2013>
2020-01-13 11:39:20 +0400

    Extended Swagger2 for documentation

--- src/main/java/com/example/restfulwebservices/user/User.java
+++ src/main/java/com/example/restfulwebservices/user/User.java
@@ -1,14 +1,19 @@
 package com.example.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel("All details about the user.")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty()
     private String name;
 
     @Past
